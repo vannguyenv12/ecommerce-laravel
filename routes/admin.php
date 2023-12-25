47b3b4ba92ef3cf348e980756ca1536fc1deca0e
vannguyenv12 <npnv.vn1@gmail.com>
2023-12-25 16:30:56 +0700

    Feat: Backend Shipping Rule

--- routes/admin.php
+++ routes/admin.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Backend\ProductVariantItemController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SellerProductController;
 use App\Http\Controllers\Backend\SettingController;
+use App\Http\Controllers\Backend\ShippingRuleController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use Illuminate\Support\Facades\Route;
@@ -80,6 +81,9 @@ Route::delete('flash-sale/{id}', [FlashSaleController::class, 'destroy'])->name(
 // Coupon
 Route::put('coupons/change-status', [CouponController::class, 'changeStatus'])->name('coupons.change-status');
 Route::resource('coupons', CouponController::class);
+// Shipping Rule
+Route::put('shipping-rule/change-status', [ShippingRuleController::class, 'changeStatus'])->name('shipping-rule.change-status');
+Route::resource('shipping-rule', ShippingRuleController::class);
 
 // Setting
 Route::get('settings', [SettingController::class, 'index'])->name('settings.index');

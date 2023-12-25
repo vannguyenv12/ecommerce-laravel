1f7376b74bdaeca036b51ea965efcc59c10803c2
vannguyenv12 <npnv.vn1@gmail.com>
2023-12-25 17:51:35 +0700

    feat: Frontend Multi Address

--- resources/views/frontend/dashboard/layouts/master.blade.php
+++ resources/views/frontend/dashboard/layouts/master.blade.php
@@ -5,7 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <title>One Shop || e-Commerce HTML Template</title>
   <link rel="icon" type="image/png" href="images/favicon.png">
   <link rel="stylesheet" href="{{asset('frontend/css/all.min.css')}}">
@@ -103,6 +105,8 @@
   <!--main/custom js-->
   <script src="{{asset('frontend/js/main.js')}}"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
@@ -110,6 +114,61 @@
             @endforeach
         @endif
     </script>
+
+
+<script>
+    $(document).ready(function() {
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    $('body').on('click', '.delete-item', function(event) {
+        event.preventDefault();
+
+        let deleteUrl = $(this).attr('href');
+        console.log(deleteUrl)
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+            if (result.isConfirmed) {
+
+                $.ajax({
+                    type: 'DELETE',
+                    url: deleteUrl,
+                    success: function(data) {
+                        if (data.status == 'success') {
+                            Swal.fire('Deleted!', data.message, 'success');
+                            window.location.reload();
+
+                        }
+
+                        if (data.status == 'error') {
+                            Swal.fire('Cannot Deleted!', data.message, 'error');
+                        }
+
+                    },
+                    error: function(xhr, status, error) {
+                        console.log(error);
+                    }
+                })
+
+
+            }
+            });
+    })
+})
+  </script>
+
 </body>
 
 </html>
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -3,9 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
         rel="stylesheet">
     <title>Sazao || e-Commerce HTML Template</title>
     <link rel="icon" type="image/png" href="{{asset('frontend/images/favicon.png')}}">
@@ -147,6 +150,7 @@
     <!--main/custom js-->
     <script src="{{asset('frontend/js/main.js')}}"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
     <script>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
